4b7e952cfc5c2e8652b71b729765db5fa02a4b0b
Heera <92657209+mykuromi@users.noreply.github.com>
2023-09-14 00:34:13 +0900

    12 글 생성

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,3 +1,40 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-  return <>Create!</>;
+  const router = useRouter();
+  return (
+    // 사용자와 사용하는 것은 server component에서 다루지 않음 => client component
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const title = e.target.title.value;
+        const body = e.target.body.value;
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title, body }),
+        };
+        fetch(`http://localhost:9999/topics`, options)
+          .then((res) => res.json())
+          .then((result) => {
+            const lastid = result.id;
+            router.push(`/read/${lastid}`);
+          });
+      }}
+    >
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
